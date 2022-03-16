f3fce646906b4a33e5bcfdda4cc74dceadbe8784
Tony Tran <stran8089@gmail.com>
2022-03-16 14:14:42 -0400

    added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
     PropertyCardComponent,
-    PropertyListComponent
-  ],
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
