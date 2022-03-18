2ebadd97035a79485269a8084a311f65f38b3de3
Tony Tran <stran8089@gmail.com>
2022-03-17 22:23:41 -0400

    update connection string to use remote api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,18 +10,28 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   addLoan(loan: Loan) {
-    return this.http.post<Loan>('https://localhost:7066/api/Loans', loan);
+    return this.http.post<Loan>(
+      'https://loansapi.azurewebsites.net/api/loans',
+      loan
+    );
   }
 
   getLoans(): Observable<Loan[]> {
-    return this.http.get<Loan[]>('https://localhost:7066/api/Loans');
+    return this.http.get<Loan[]>(
+      'https://loansapi.azurewebsites.net/api/loans'
+    );
   }
 
   updateLoan(id: number, loan: Loan) {
-    return this.http.put<Loan>('https://localhost:7066/api/Loans/' + id, loan);
+    return this.http.put<Loan>(
+      'https://loansapi.azurewebsites.net/api/loans/' + id,
+      loan
+    );
   }
 
   deleteLoan(id: number) {
-    return this.http.delete<Loan>('https://localhost:7066/api/Loans/' + id);
+    return this.http.delete<Loan>(
+      'https://loansapi.azurewebsites.net/api/loans/' + id
+    );
   }
 }
